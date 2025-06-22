7050ce601484781f61376a5dacae312a68031ea2
ram_swaroop <ramswaroopbhakar14@gmail.com>
2025-06-22 10:17:05 +0530

    config: correct model config as per .env.example

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(".env.example") #replace with your actual .env file if needed
 
-OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "qwen2.5:3b")
+OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
+
+# print(f"Using Ollama model: {OLLAMA_MODEL}")
