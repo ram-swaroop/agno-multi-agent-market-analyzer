f17105d19003055fffbc9e84b9de8e32670c8b3e
ram_swaroop <ramswaroopbhakar14@gmail.com>
2025-06-22 12:15:22 +0530

    config: llm model change

--- config/settings.py
+++ config/settings.py
@@ -6,8 +6,8 @@ from agno.models.google import Gemini
 load_dotenv(".env") #refer .env.example to set configuration 
 
 # Model-1: IF using Ollama model, uncomment the following line:
-model_config = Ollama(id=os.getenv("OLLAMA_MODEL"), provider="Ollama")
+# model_config = Ollama(id=os.getenv("OLLAMA_MODEL"), provider="Ollama")
 
 # Model-2: IF using Google Gemini model, uncomment the following lines:
-# os.environ["GOOGLE_API_KEY"] = os.getenv("GEMINI_API_KEY")
-# model_config = Gemini(id=os.getenv("GEMINI_MODEL"), provider="Google")
+os.environ["GOOGLE_API_KEY"] = os.getenv("GEMINI_API_KEY")
+model_config = Gemini(id=os.getenv("GEMINI_MODEL"), provider="Google")
